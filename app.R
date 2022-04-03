7dfdb65b2bad7692e1d15ec661131ecfe677b0e2
jawhitelaw <jwhitela@macalester.edu>
2022-04-02 20:00:08 -0500

    final commit

--- app.R
+++ app.R
@@ -1,33 +1,38 @@
 library(shiny)
 library(tidyverse)
-library(babynames)
+
+covid19 <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
+census_pop_est_2018 <- read_csv("https://www.dropbox.com/s/6txwv3b4ng7pepe/us_census_2018_state_pop_est.csv?dl=1") %>% 
+  separate(state, into = c("dot","state"), extra = "merge") %>% 
+  select(-dot) %>% 
+  mutate(state = str_to_title(state))
+
+covid19_clean <- covid19 %>% 
+  left_join(census_pop_est_2018, by = c("state" = "state")) %>% 
+  mutate(cases_per_100k = (cases/est_pop_2018)*100000)
 
 ui <- fluidPage(
-  sliderInput(inputId = "years", 
-              label = "Year Range",
-              min = 1880, 
-              max = 2019, 
-              value = c(1880,2019),
-              sep = ""),
-  textInput("name", 
-            "Name", 
-            value = "", 
-            placeholder = "Lisa"),
-  selectInput("sex", 
-              "Sex", 
-              choices = list(Female = "F", Male = "M")),
+  sliderInput("Dates",
+              "Dates:",
+              min = as.Date("2020-01-21","%Y-%m-%d"),
+              max = as.Date("2022-03-31","%Y-%m-%d"),
+              value=as.Date(c("2020-01-21","2022-03-31")),
+              timeFormat="%Y-%m-%d"),
+  selectInput("state", 
+              "State", 
+              choices = unique(covid19$state), multiple = TRUE),
   submitButton(text = "Create my plot!"),
   plotOutput(outputId = "timeplot")
 )
 
 server <- function(input, output) {
   output$timeplot <- renderPlot({
-    babynames %>% 
-      filter(name == input$name, 
-             sex == input$sex) %>% 
+    covid19_clean %>% 
+      filter(state %in% input$state) %>% 
       ggplot() +
-      geom_line(aes(x = year, y = n)) +
-      scale_x_continuous(limits = input$years) +
+      geom_line(aes(x = date, y = cases_per_100k, color = state)) +
+      scale_x_continuous(limits = input$Dates) +
+      labs(x = "Date", y = "Cases per 100,000 residents") +
       theme_minimal()
   })
 }
